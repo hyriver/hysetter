05bec3b3f34c4be69380b1fd6999621652c1b854
Taher Chegini <cheginit@gmail.com>
2025-05-24 18:33:38 -0400

    chore: separet the test into two tests so they are shown individually. [skip ci]

--- tests/test_hysetter.py
+++ tests/test_hysetter.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import io
 
 import pytest
-from click.testing import CliRunner
+from click.testing import CliRunner, Result
 
 import hysetter as hs
 from hysetter.cli import cli
@@ -15,9 +15,17 @@ def runner():
     return CliRunner()
 
 
-def test_config(runner: CliRunner) -> None:
+@pytest.fixture
+def run_config_once(runner: CliRunner) -> Result:
     ret = runner.invoke(cli, ["config_demo.yml"])
-    assert ret.exit_code == 0
+    return ret
+
+
+def test_config(run_config_once: Result) -> None:
+    assert run_config_once.exit_code == 0
+
+
+def test_config_overwrite(runner: CliRunner, run_config_once: Result) -> None:
     ret = runner.invoke(cli, ["config_demo.yml", "--overwrite"])
     assert ret.exit_code == 0
 
